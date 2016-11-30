38465675b10e0fb7f8155c83d1567caf02edf725
lintbe <jfdusar@gmail.com>
2016-11-30 18:18:02 +0100

    game states early draft

---
+++ src/stateBlocks/levelSelect.asm
@@ -0,0 +1,39 @@
+;
+; Kung Fu Master Level Select state
+;
+; by lintbe/AntiheroSoftware <jfdusar@gmail.com>
+;
+
+            .setcpu     "65816"
+            .feature	c_comments
+
+            .include    "snes.inc"
+            .include	"states.def"
+
+            .export 	levelSelectStateDef
+
+.segment "RODATA"
+
+levelSelectStateDef:
+	.word #.LOWORD(levelSelectInit)
+	.word #.LOWORD(levelSelectBefore)
+	.word #.LOWORD(levelSelectMain)
+	.word #.LOWORD(levelSelectAfter)
+	.word #.LOWORD(levelSelectDestroy)
+
+.segment "CODE"
+
+.proc levelSelectInit
+.endproc
+
+.proc levelSelectBefore
+.endproc
+
+.proc levelSelectMain
+.endproc
+
+.proc levelSelectAfter
+.endproc
+
+.proc levelSelectDestroy
+.endproc
